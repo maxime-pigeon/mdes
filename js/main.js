9e60fad13a8eaee1838c4ba79bef44505003d116
BeauAnto <antoine.beauchesne@outlook.com>
2022-04-07 19:28:53 -0400

    Schedule almost complete
    - Patrick and Lindsey events are still not 100% confirm
    -The trailer is also still not present

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Manage collapsible buttons for the Schedule Section
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -11,4 +12,32 @@ for (i = 0; i < coll.length; i++) {
       content.style.display = "block";
     }
   });
+}
+
+// Manage the display of the overlay in the Schedule Section
+  // Overlay 1
+document.getElementById("overlay1B").onclick = function ShowOverlay1() {
+  document.getElementById("overlay1").style.display = "block";
+}
+
+document.getElementById("overlay1CloseB").onclick = function HideOverlay1() {
+  document.getElementById("overlay1").style.display = "none";
+}
+
+  // Overlay 2
+document.getElementById("overlay2B").onclick = function ShowOverlay2() {
+  document.getElementById("overlay2").style.display = "block";
+}
+
+document.getElementById("overlay2CloseB").onclick = function HideOverlay2() {
+  document.getElementById("overlay2").style.display = "none";
+}
+
+  // Overlay 3
+document.getElementById("overlay3B").onclick = function ShowOverlay3() {
+  document.getElementById("overlay3").style.display = "block";
+}
+
+document.getElementById("overlay3CloseB").onclick = function HideOverlay3() {
+  document.getElementById("overlay3").style.display = "none";
 }
